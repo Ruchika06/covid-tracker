7a64f64605e55a68099f06f937bccc6c14d96d38
Riya65 <rhika6sharan@gmail.com>
2020-08-10 22:06:56 +0530

    Updates

---
+++ precache-manifest.dc1eb71d6742051e0d94a85ecee2011f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faa280dedbcf49433d34777abfa2f01b",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "4b22ed52d54186cdb070",
+    "url": "/covid-tracker/static/css/main.13c8a689.chunk.css"
+  },
+  {
+    "revision": "fbab5062462b2878939d",
+    "url": "/covid-tracker/static/js/2.9fcf19fd.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-tracker/static/js/2.9fcf19fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b22ed52d54186cdb070",
+    "url": "/covid-tracker/static/js/main.b79edbfe.chunk.js"
+  },
+  {
+    "revision": "ef7fedef9cd7db49b0f7",
+    "url": "/covid-tracker/static/js/runtime-main.adeb3781.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-tracker/static/media/image.d7265326.png"
+  }
+]);
